b1904aeb0615f8f39b7cd1059783dc8ced1b79e0
Nandini Mundhra <nandinimundhra7@gmail.com>
2026-08-14 17:41:06 +0530

    implement the websocket server

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -48,7 +48,9 @@ matchRouter.post('/', async (req,res) =>{
             awayScore: awayScore??0,
             status:getMatchStatus(startTime,endTime),
         }).returning();
-
+        if(res.app.locals.broadcastMatchCreated){
+            res.app.locals.broadcastMatchCreated(event);
+        }
         res.status(200).json({data:event});
 
      }catch (e) {
